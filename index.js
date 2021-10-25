315c246261321a84baacf68f801d1b0b004b4b62
Luis Ramirez <luis21x@outlook.com>
2021-10-25 13:24:14 -0400

    creating starter files

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+var createREADME = arr => {
+
+}
+
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            message: '',
+            name: '',
+        },
+    ])
