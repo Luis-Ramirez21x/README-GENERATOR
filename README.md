30efd9a6c1bdfea090bfdf6ac0ad6d5f4c997d57
Luis Ramirez <luis21x@outlook.com>
2021-10-25 13:53:23 -0400

    adding promts and user input questions. As well as creating template for read me

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # README-GENERATOR
+# ${project.name}
+
+## Project Description
+#### ${project.description}
+
+## Table of Contents
+* Installation
+* Usage
+* Contributing
+* Tests
+* Questions
+* License
+
+## Installation 
+#### ${project.installInstructions}
+
+## Usage
+#### ${project.usageInfo}
+
+## Contributing
+#### ${project.contributionGuidelines}
+
+## Tests
+#### ${project.testInstructions}
+
+## Questions
+#### ${project.githubUserName}
+
+## License
+
--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-var createREADME = arr => {
-
+var createREADME = project => {
+    return ``
+    
 }
 
 inquirer 
     .prompt([
         {
             type: 'input',
-            message: '',
-            name: '',
+            message: 'What is the name of your project?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Describe your project',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions',
+            name: 'installInstructions',
+        },
+        {
+            type: 'input',
+            message: 'Enter usage information',
+            name: 'usageInfo',
+        },
+        {
+            type: 'input',
+            message: 'Enter contribution guidelines',
+            name: 'contributionGuidelines',
+        },
+        {
+            type: 'input',
+            message: 'Enter Test Intructions',
+            name: 'testInstructions',
+        },
+        {
+            type: 'input',
+            message: 'Enter your github username',
+            name: 'githubUserName',
         },
     ])
