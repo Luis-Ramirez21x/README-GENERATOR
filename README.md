00e9a6475eee2546b8789c22663f6dc6c88348d3
Luis Ramirez <luis21x@outlook.com>
2021-10-26 21:24:29 -0400

    adding on badge option feature as well as badge emblem creation

--- README.md
+++ README.md
@@ -1,32 +1,33 @@
-# README-GENERATOR
-# my 
+[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+# nba compare
     
 ## Project Description
-#### name 
+#### seearch players and compare
     
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
-* Contributing
-* Tests
-* Questions
-* License
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
     
 ## Installation 
-#### is 
+####  npm install
     
 ## Usage 
-#### luis
+#### use wisely
     
 ## Contributing
-#### i 
+#### none
     
 ## Tests
-#### was 
+#### testing
     
 ## Questions
-#### http://github.com/born 
-#### Please email me at in  for an additional questions.
+#### http://github.com/luis21x
+#### Please email me at luis21xs3 for an additional questions.
     
 ## License
+#### This application is covered under the Boost Software License 1.0.
     
--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 var createREADME = project => {
-    return `# README-GENERATOR
+    let licenseURL = '';
+    switch(project.license){
+        case 'Apache 2.0 License':
+            licenseURL = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            break;
+        case 'Boost Software License 1.0':
+            licenseURL = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+            break;
+        case 'BSD 3-Clause License':
+            licenseURL = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+            break;
+        case 'BSD 2-Clause License':
+            licenseURL = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+            break;
+    
+   }
+
+    return `${licenseURL}
 # ${project.name}
     
 ## Project Description
@@ -11,10 +28,10 @@ var createREADME = project => {
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
-* Contributing
-* Tests
-* Questions
-* License
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
     
 ## Installation 
 #### ${project.installInstructions}
@@ -33,9 +50,12 @@ var createREADME = project => {
 #### Please email me at ${project.email} for an additional questions.
     
 ## License
+#### This application is covered under the ${project.license}.
     `
     
 }
+
+
 
 inquirer 
     .prompt([
@@ -78,6 +98,12 @@ inquirer
             type: 'input',
             message: 'Enter your email',
             name: 'email',
+        },
+        {
+            type: 'rawlist',
+            message: 'Select your license',
+            name: 'license',
+            choices: ['Apache 2.0 License','Boost Software License 1.0','BSD 3-Clause License','BSD 2-Clause License'],
         }
     ])
     .then((response) => 
